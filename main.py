519d5bef55fa3bea72e63952aafa2575c09a1beb
Sebastian Stüwe <sebastian.stuewe@mailbox.org>
2026-02-27 13:46:23 +0100

    chore: Splash-Version auf 0.4.8

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from gui.mainwindow import MainWindow
 
 # --- App-Metadaten ---
 APP_NAME = "Schild Spider"
-APP_VERSION = "0.4.5"
+APP_VERSION = "0.4.8"
 APP_COPYRIGHT = "© 2025–2026"
 APP_LICENSE = "GPL v3"
 
